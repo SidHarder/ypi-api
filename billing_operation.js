473e54b31ee172eabb6a993ac734ebd29f207c3f
SidHarder <sideharder@gmail.com>
2021-07-13 03:02:58 +0000

    adding billing

--- billing_operation.js
+++ billing_operation.js
@@ -7,6 +7,9 @@ function processBillingOperation(args, cb) {
     case 'getCharges':
       getCharges(args[0].billingOperation, cb)
       break
+    case 'getClientBillingDetailReport':
+      getClientBillingDetailReport(args[0].billingOperation, cb)
+      break
     default:
       cb(null, { status: 'ERROR', message: 'Method Not Found.' })
       break
@@ -21,6 +24,33 @@ function getCharges(args, cb) {
   })
 }
 
+function getClientBillingDetailReport(args, cb) {
+  var startDate = args.startDate;
+  if (!startDate) {
+    console.log(`A start date was not provided as an argument.`);
+    return cb(null, { status: 'ERROR', message: `A start date was not provided as an argument.` });
+  }
+
+  var endDate = args.endDate;
+  if (!endDate) {
+    console.log(`An end date was not provided as an argument.`);
+    return cb(null, { status: 'ERROR', message: `An end date was not provided as an argument.` });
+  }
+
+  var clientGroupId = args.clientGroupId;
+  if (!clientGroupId) {
+    console.log(`A client group Id was not provided as an argument.`);
+    return cb(null, { status: 'ERROR', message: `A client group Id was not provided as an argument.` });
+  }
+
+  var sql = `call prcGetClientBillingDetailReport_2('${startDate}', '${endDate}', ${clientGroupId})`;
+  db.executeSqlCommand(sql, function (error, result) {
+    if (error) return cb(null, error);
+    cb(null, result);
+  })
+}
+
 billingOperation.getCharges = getCharges;
 billingOperation.processBillingOperation = processBillingOperation;
+billingOperation.getClientBillingDetailReport;
 module.exports = billingOperation;
