9c5f4a5fe0adad2542e2aebaaa49bcf2fef46a41
SidHarder <sideharder@gmail.com>
2021-09-27 18:03:04 +0000

    adding first name to text/email

--- covid_result_distribution.js
+++ covid_result_distribution.js
@@ -5,48 +5,56 @@ var text = require('./text_message')
 
 var covidResultDistribution = {};
 
-function sendText(args, cb) {
-  console.log('Running sendText')  
+function sendText(args, cb) {  
   const phoneNumber = args[0].phoneNumber;
-  if (!phoneNumber) {
-    console.log(`The phone number was not provided as an argument.`);
+  if (!phoneNumber) {  
     return cb(null, { status: 'ERROR', message: `The phone number was not provided as an argument` });
   }
 
   var reportNo = args[0].reportNo;
-  if (!reportNo) {
-    console.log(`A reportNo was not provided as an argument.`);
+  if (!reportNo) {    
     return cb(null, { status: 'ERROR', message: `A reportNo was not provided as an argument.` });
   }
 
+  var firstName = args[0].firstName;
+  if (!firstName) {
+    return cb(null, { status: 'ERROR', message: `A First Name was not provided as an argument.` });
+  }
+
   encryptionOperation.createResultUrl([{ reportNo: reportNo }], function (error, result) {       
-    var message = `This is Yellowstone Pathology Institute.  Your SARS-CoV-2 PCR test result is ready.  Please click on the following link to review your result: ${result.url}`;
+    var message = `${firstName}, this is Yellowstone Pathology Institute.  Your SARS-CoV-2 PCR test result is ready.  Please click on the following link to review your result: ${result.url}`;
     textMessage.send([{ phoneNumber: phoneNumber, message: message }], function (error, result) {      
       cb(null, { status: 'OK', message: 'A text was sent.' })
     })    
   })  
 }
 
-function sendEmail(args, cb) {
-  console.log('Running sendEmail')  
+function sendEmail(args, cb) {  
   const emailAddress = args[0].emailAddress;
-  if (!emailAddress) {
-    console.log(`The email address was not provided as an argument.`);
+  if (!emailAddress) {    
     return cb(null, { status: 'ERROR', message: `The email address was not provided as an argument` });
   }
 
   var reportNo = args[0].reportNo;
-  if (!reportNo) {
-    console.log(`A reportNo was not provided as an argument.`);
+  if (!reportNo) {    
     return cb(null, { status: 'ERROR', message: `A reportNo was not provided as an argument.` });
+  }
+
+  var firstName = args[0].firstName;
+  if (!firstName) {
+    return cb(null, { status: 'ERROR', message: `A First Name was not provided as an argument.` });
   }
 
   encryptionOperation.createResultUrl([{ reportNo: reportNo }], function (error, result) {
     var subject = `Your SARS-CoV-2 PCR test result is ready.`    
-    var message = `This is Yellowstone Pathology Institute.  Your SARS-CoV-2 PCR test result is ready.  Please click on the following link to review your result: <a href=${result.url}>${result.url}</a>`;
+    var message = `${firstName}, This is Yellowstone Pathology Institute.  Your SARS-CoV-2 PCR test result is ready.  Please click on the following link to review your result: <a href=${result.url}>${result.url}</a>`;
     email.send([{ emailAddress: emailAddress, message: message, subject: subject }], function (error, result) {  
-      console.log(result)    
-      cb(null, { status: 'OK', message: 'An email was sent.' })
+      if(error) {
+        console.error(error);
+        cb(null, { status: 'ERROR', message: error })
+      } else {
+        cb(null, { status: 'OK', message: 'An email was sent.' })
+      }      
     })
   })  
 }
