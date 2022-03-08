24d3fa6937b23cc38b38c5f12ec50526834e0137
sidharder <sideharder@gmail.com>
2022-03-07 18:34:41 -1000

    new version

--- application_version.js
+++ application_version.js
@@ -14,7 +14,8 @@ versions.push({ version: '1.1.2.0', updateAvailable: true, applicationName: 'YPI
 versions.push({ version: '1.1.1.0', updateAvailable: true, applicationName: 'YPIConnect' });
 
 //YPI LIS Versions
-versions.push({ version: '1.0.0.3', updateAvailable: false, applicationName: 'YPILIS' });
+versions.push({ version: '1.0.0.4', updateAvailable: false, applicationName: 'YPILIS' });
+versions.push({ version: '1.0.0.3', updateAvailable: true, applicationName: 'YPILIS' });
 versions.push({ version: '1.0.0.2', updateAvailable: true, applicationName: 'YPILIS' });
 versions.push({ version: '1.0.0.1', updateAvailable: true, applicationName: 'YPILIS' });
 versions.push({ version: '1.0.0.0', updateAvailable: true, applicationName: 'YPILIS' });
