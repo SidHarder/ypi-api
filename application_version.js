5102fb800d3b6fefa11c065a185113b59029e88d
sidharder <sideharder@gmail.com>
2022-03-15 21:00:44 -1000

    upding version

--- application_version.js
+++ application_version.js
@@ -2,7 +2,8 @@ var aplicationVersion = {};
 
 var versions = []
 //YPI Connect Versions
-versions.push({ version: '2.0.0.0', updateAvailable: false, applicationName: 'YPIConnect' });
+versions.push({ version: '2.0.0.1', updateAvailable: false, applicationName: 'YPIConnect' });
+versions.push({ version: '2.0.0.0', updateAvailable: true, applicationName: 'YPIConnect' });
 
 //YPI LIS Versions
 versions.push({ version: '1.0.0.4', updateAvailable: false, applicationName: 'YPILIS' });
